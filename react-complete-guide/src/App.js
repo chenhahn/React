fbe0a89f38c2211f8ff152f79ab5a8ab547458b4
hahn <hahn2017@my.yorku.ca>
2020-05-06 22:07:56 -0400

    commit

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -33,9 +33,16 @@ const app = props => {
             <h1>Hi, I'm a React App</h1>
             <p>This is really working!</p>
             <button onClick={switchNameHandler}>Switch Name</button>
-            <Person name={personsState.persons[0].name} age={personsState.persons[0].age} />
-            <Person name={personsState.persons[1].name} age={personsState.persons[1].age}>My Hobbies: Racing</Person>
-            <Person name={personsState.persons[2].name} age={personsState.persons[2].age} />
+            <Person
+                name={personsState.persons[0].name}
+                age={personsState.persons[0].age} />
+            <Person
+                name={personsState.persons[1].name}
+                age={personsState.persons[1].age}
+                click={this.switchNameHandler} >My Hobbies: Racing</Person>
+            <Person
+                name={personsState.persons[2].name}
+                age={personsState.persons[2].age} />
         </div>
     );
     // return React.createElement('div',{className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
